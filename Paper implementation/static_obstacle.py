ccbdcda79356b07ba94a754860938f0d8fb267c2
ntran26 <hoangnamtran2610@gmail.com>
2024-06-09 23:12:56 +1000

    update

--- Paper implementation/static_obstacle.py
+++ Paper implementation/static_obstacle.py
@@ -19,9 +19,10 @@ OBSTACLE_RADIUS = SQUARE_SIZE/3
 WIDTH = 50
 HEIGHT = 50
 START = (0, 0)
+GOAL = (0, 200)
 TURN_RATE = 5
 INITIAL_HEADING = 90
-STEP = 60
+STEP = 100
 
 class asv_visualization:
     # Initialize environment
@@ -56,17 +57,6 @@ class asv_visualization:
         self.left_heading = [self.heading]
         self.position = self.start_pos
 
-        # # Turn left
-        # while self.step_count < self.step:
-        #     self.position = (self.position[0] + self.speed * np.cos(np.radians(self.current_heading)),
-        #                      self.position[1] + self.speed * np.sin(np.radians(self.current_heading)))
-        #     # Append new position and heading angle to list
-        #     self.left_path.append(self.position)
-        #     self.left_heading.append(self.current_heading)
-        #     # Update new heading angle and step count
-        #     self.current_heading += self.turn_rate
-        #     self.step_count += 1
-
         # Go straight
         while self.step_count < self.step:
             self.position = (self.position[0] + self.speed * np.cos(np.radians(self.current_heading)),
@@ -79,7 +69,7 @@ class asv_visualization:
             self.step_count += 1
 
         # Define and obstacles
-        static_obstacles = [(-30, -40), (70, -60)]      
+        static_obstacles = [(-30, -40), (70, -60), (70, 70), (0, 150)]      
 
         # Initialize figure and axes
         fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
@@ -93,10 +83,17 @@ class asv_visualization:
         ax1.add_patch(observation_horizon1)
         ax2.add_patch(observation_horizon2)
 
-        # Plot the path
-        path_array = np.array(self.left_path)
-        ax1.plot(path_array[:,0], path_array[:,1], '-', color=RED)
+        # Generate the path
+        self.path = []
+        for y in range(START[1], GOAL[1]+1):
+            self.path.append((START[0], y))
 
+        print(self.path)
+
+        # Plot the path
+        path_array = np.array(self.path)
+        ax1.plot(path_array[:,0], path_array[:,1], '-', color=GREEN)
+        
         # Plot obstacles
         for (x, y) in static_obstacles:
             ax1.plot(x, y, marker='o', color=RED)
@@ -156,14 +153,25 @@ class asv_visualization:
                 # Update the collision grid on the second plot
                 ax2.add_patch(rect)
                 squares.append(rect)
+            
+            for (cx, cy) in grid:
+                is_path = any(np.sqrt((cx - ox)**2 + (cy - oy)**2) < (SQUARE_SIZE/2 + OBSTACLE_RADIUS)
+                              for ox, oy in self.path)
+                # Change the color of the grid if there is path
+                color = 'green' if is_path else 'none'
+                rect = plt.Rectangle((cx - SQUARE_SIZE/2, cy - SQUARE_SIZE/2), SQUARE_SIZE, SQUARE_SIZE,
+                                     edgecolor='gray', facecolor=color)
+                # Update the collision grid
+                ax2.add_patch(rect)
+                squares.append(rect)
 
             return self.agent_1, self.agent_2, observation_horizon1, observation_horizon2, *squares
 
         ani = FuncAnimation(fig, update, frames=len(self.left_path), init_func=init, blit=True, interval=200, repeat=False)
         ax1.set_xlim(-RADIUS - 50, RADIUS + 50)
-        ax1.set_ylim(-RADIUS - 50, RADIUS + 50)
+        ax1.set_ylim(-RADIUS - 50, RADIUS + 200)
         ax2.set_xlim(-RADIUS - 50, RADIUS + 50)
-        ax2.set_ylim(-RADIUS - 50, RADIUS + 50)
+        ax2.set_ylim(-RADIUS - 50, RADIUS + 200)
 
         # # Write to mp4 file
         # FFwriter = FFMpegWriter(fps=5)
