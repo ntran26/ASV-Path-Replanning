1121d29413c927762e7645dc9c1caf6970dae06e
ntran26 <hoangnamtran2610@gmail.com>
2024-07-31 18:12:42 +1000

    update

--- Paper implementation/static_obs_env_new.py
+++ Paper implementation/static_obs_env_new.py
@@ -1,5 +1,6 @@
 import numpy as np
 import gymnasium as gym
+from gymnasium import spaces
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation, FFMpegWriter
 
@@ -38,10 +39,41 @@ GOAL_STATE = 2
 COLLISION_STATE = 3
 
 class ASVEnv:
-    def __init__(self):
+    metadata = {"render_modes": ["human"], "render_fps": 10}
+    def __init__(self, render_mode = "human"):
         super(ASVEnv, self).__init__()
+        self.render_mode = render_mode
+        self.width = WIDTH
+        self.height = HEIGHT
+        self.boundary = [(0,0), (0,self.height), (self.width,self.height), (self.width,0), (0,0)]
+        self.position = START
+        self.goal = GOAL
+        self.heading = INITIAL_HEADING
+        self.speed = SPEED
+        self.turn_rate = TURN_RATE
+
+        self.action_space = spaces.Discrete(3)
+        self.observation_space = spaces.Box()   # ***to be updated***
     
-    def reset(self):
+    # Generate grid function
+    def generate_grid(self, radius, square_size, center):
+        x = np.arange(-radius + square_size, radius, square_size)
+        y = np.arange(-radius + square_size, radius, square_size)
+        grid = []
+        for i in x:
+            for j in y:
+                if np.sqrt(i**2 + j**2) <= radius:
+                    grid.append((center[0] + i, center[1] + j))
+        return grid
+
+    def reset(self, seed=None, options=None):
+        super().reset(seed=seed)
+
+        self.position = START
+        self.heading = INITIAL_HEADING
+
+
+
         return
     
     def is_on_path(self, position):
