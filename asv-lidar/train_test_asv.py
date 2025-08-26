28c18e01da37035a832380d60d52c4e1abd43da8
ntran26 <hoangnamtran2610@gmail.com>
2025-08-27 07:24:50 +1000

    update

--- asv-lidar/train_test_asv.py
+++ asv-lidar/train_test_asv.py
@@ -150,7 +150,7 @@ if __name__=='__main__':
             # model = SAC.load(MODEL_PATH)
             # model = SAC.load("models/sac_asv_model_v1.zip")
             # model = SAC.load("models/sac_asv_model_v2.zip")
-            model = SAC.load("models/sac_asv_model_0_9.zip")
+            model = SAC.load("models/sac_asv_model_0_1.zip")
 
         env = testEnv(render_mode="human")
         env.test_case = args.case
